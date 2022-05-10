ee3e69936dc404a1845ee8aee4be74905ced569d
Kaan Bozkuş <98909523+kaanbozkus@users.noreply.github.com>
2022-05-11 01:22:39 +0300

    Problem 7 itself is added

--- Project Euler/Problem7.js
+++ Project Euler/Problem7.js
@@ -1,3 +1,7 @@
+/* By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number? */
+
 let isPrime = function (number) {
   for (let i = 2; i <= Math.ceil(Math.sqrt(number)); i++) {
     if (number % i == 0) {
